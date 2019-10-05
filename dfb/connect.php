d1b43d7703c971722f350764bc6e533da5927351
Boidushya <boidushyabhattacharya@gmail.com>
2019-10-05 19:39:23 +0530

    init

---
+++ dfb/connect.php
@@ -0,0 +1,45 @@
+<?php
+$toptext = filter_input(INPUT_POST, 'toptext');
+$bottomtext = filter_input(INPUT_POST, 'bottomtext');
+$submitter = filter_input(INPUT_POST, 'submitter');
+if (!empty($toptext)){
+if (!empty($bottomtext)){
+if (!empty($submitter)){
+$host = "localhost";
+$dbusername = "user";
+$dbpassword = "pass";
+$dbname = "demodeep";
+// Create connection
+$conn = new mysqli ($host, $dbusername, $dbpassword, $dbname);
+if (mysqli_connect_error()){
+die('Connect Error ('. mysqli_connect_errno() .') '
+. mysqli_connect_error());
+}
+else{
+$sql = "INSERT INTO submissions (toptext, bottomtext, submitter)
+values ('$toptext','$bottomtext','$submitter')";
+if ($conn->query($sql)){
+echo "Submitted sucessfully!";
+}
+else{
+echo "Error: ". $sql ."
+". $conn->error;
+}
+$conn->close();
+}
+}
+else{
+echo "Submitter's name cannot be empty";
+die();
+}
+}
+else{
+echo "Bottom Text cannot be empty";
+die();
+}
+}
+else{
+echo "Top Text cannot be empty";
+die();
+}
+?>
